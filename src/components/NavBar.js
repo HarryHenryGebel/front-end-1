6423a120d132c2034d27c106778e67db72761ac6
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-24 16:06:43 -0700

    definitely not something being kept, but working out how to nest the diff reactstrap components to create  what we wamt. 100% ugly code at the moment, but I'm learning how to mix n match

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,30 @@
 import React, { useState } from "react"
 import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText} from "reactstrap"
+import { TabContent, TabPane, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
 
 export default function NavBar () {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
+    const [collapsed, setCollapsed] = useState(true);
+    const toggleNavbar = () => setCollapsed(!collapsed);
+
+    const [activeTab, setActiveTab] = useState('1');
+    const toggleTab = tab => {
+        if(activeTab !== tab) setActiveTab(tab);
+    }
+
 
     return (
         <div>
+            {/*
             <Navbar color="light" light expand="md">
                 <NavbarBrand href="/">Not Yo Momma's Potluck</NavbarBrand>
                 <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar>
+                <Collapse isOpen={isOpen} navbar> */}
                 {/*add a space here pushing to opposite sides of screen */}
-                    <Nav className="mr-auto" navbar>
+                    {/* <Nav className="mr-auto" navbar>
                         <UncontrolledDropdown nav inNavbar>
                         <DropdownToggle nav caret>
                             More Info (marketing pgs)
@@ -36,10 +47,72 @@ export default function NavBar () {
                         </NavItem>
                         <NavItem>
                         <NavLink href="https://github.com/reactstrap/reactstrap">Register</NavLink>
-                        </NavItem>
+                        </NavItem> */}
                         {/* Dynamic Dropdown that only appears after login/registration */}
-                    </Nav>
+                    {/* </Nav>
+                </Collapse>
+            </Navbar> */}
+ <Nav tabs>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '1' })}
+            onClick={() => { toggleTab('1'); }}
+          >
+            Tab1
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink
+            className={classnames({ active: activeTab === '2' })}
+            onClick={() => { toggleTab('2'); }}
+          >
+            More Tabs
+          </NavLink>
+        </NavItem>
+      </Nav>
+      <TabContent activeTab={activeTab}>
+        <TabPane tabId="1">
+          <Row>
+            <Col sm="12">
+              <h4>Tab 1 Contents</h4>
+            </Col>
+          </Row>
+        </TabPane>
+        <TabPane tabId="2">
+          <Row>
+            <Col sm="6">
+              <Card body>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+              </Card>
+            </Col>
+            <Col sm="6">
+              <Card body>
+                <CardTitle>Special Title Treatment</CardTitle>
+                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+                <Button>Go somewhere</Button>
+              </Card>
+            </Col>
+          </Row>
+        </TabPane>
+      </TabContent>
 
+
+            <Navbar color="faded" light>
+                <NavbarBrand href="/" className="mr-auto">Not Yo Momma's Potluck</NavbarBrand>
+                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+                <Collapse isOpen={!collapsed} navbar>
+
+                <Nav navbar>
+                    <NavItem>
+                        <NavLink href="/components/">Components</NavLink>
+                    </NavItem>
+                    <NavItem>
+                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                    </NavItem>
+
+                </Nav>
                 </Collapse>
             </Navbar>
         </div>
