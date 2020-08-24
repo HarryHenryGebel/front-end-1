1f575bbcd09b681141ccbef7c3425a2dd3b9690d
Josh Glantz-Hucks <jahteo@gmail.com>
2020-08-24 16:44:29 -0700

    ok. I think I have the compnents that I need to get it together. NavBar is ready for to be broken into components & actually given functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,121 +1,87 @@
 import React, { useState } from "react"
-import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, NavbarText} from "reactstrap"
-import { TabContent, TabPane, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
-import classnames from 'classnames';
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, UncontrolledDropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 
-export default function NavBar () {
+
+export default function NavBar (props) {
     const [isOpen, setIsOpen] = useState(false);
     const toggle = () => setIsOpen(!isOpen);
 
-    const [collapsed, setCollapsed] = useState(true);
-    const toggleNavbar = () => setCollapsed(!collapsed);
-
-    const [activeTab, setActiveTab] = useState('1');
-    const toggleTab = tab => {
-        if(activeTab !== tab) setActiveTab(tab);
-    }
+    const { buttonLabel, className } = props;
+    const [modal, setModal] = useState(false);
+    const toggleModal = () => setModal(!modal);
 
 
     return (
         <div>
-            {/*
             <Navbar color="light" light expand="md">
                 <NavbarBrand href="/">Not Yo Momma's Potluck</NavbarBrand>
                 <NavbarToggler onClick={toggle} />
-                <Collapse isOpen={isOpen} navbar> */}
+                <Collapse isOpen={isOpen} navbar>
                 {/*add a space here pushing to opposite sides of screen */}
-                    {/* <Nav className="mr-auto" navbar>
+                    <Nav className="mr-auto" navbar>
                         <UncontrolledDropdown nav inNavbar>
                         <DropdownToggle nav caret>
                             More Info (marketing pgs)
                         </DropdownToggle>
                         <DropdownMenu right>
                             <DropdownItem>
-                            Option 1
+                            Testimonials
                             </DropdownItem>
                             <DropdownItem>
-                            Option 2
+                            Theme Ideas
+                            </DropdownItem>
+                            <DropdownItem>
+                            About
+                            </DropdownItem>
+                            <DropdownItem>
+                            Contact
                             </DropdownItem>
                             <DropdownItem divider />
                             <DropdownItem>
-                            Reset
+                            Home
                             </DropdownItem>
                         </DropdownMenu>
                         </UncontrolledDropdown>
+
+                        <Button color="danger" onClick={toggleModal}>{buttonLabel}</Button>
+      <Modal isOpen={modal} toggle={toggleModal} className={className}>
+        <ModalHeader toggle={toggleModal}>Modal title</ModalHeader>
+        <ModalBody>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggleModal}>Do Something</Button>{' '}
+          <Button color="secondary" onClick={toggleModal}>Cancel</Button>
+        </ModalFooter>
+      </Modal>
+
                         <NavItem>
-                        <NavLink href="/components/">Log In</NavLink>
+                            <NavLink href="">Log In</NavLink>
                         </NavItem>
                         <NavItem>
-                        <NavLink href="https://github.com/reactstrap/reactstrap">Register</NavLink>
-                        </NavItem> */}
+                           <NavLink href="">Register</NavLink>
+                        </NavItem>
                         {/* Dynamic Dropdown that only appears after login/registration */}
-                    {/* </Nav>
-                </Collapse>
-            </Navbar> */}
- <Nav tabs>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '1' })}
-            onClick={() => { toggleTab('1'); }}
-          >
-            Tab1
-          </NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink
-            className={classnames({ active: activeTab === '2' })}
-            onClick={() => { toggleTab('2'); }}
-          >
-            More Tabs
-          </NavLink>
-        </NavItem>
-      </Nav>
-      <TabContent activeTab={activeTab}>
-        <TabPane tabId="1">
-          <Row>
-            <Col sm="12">
-              <h4>Tab 1 Contents</h4>
-            </Col>
-          </Row>
-        </TabPane>
-        <TabPane tabId="2">
-          <Row>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-            <Col sm="6">
-              <Card body>
-                <CardTitle>Special Title Treatment</CardTitle>
-                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-                <Button>Go somewhere</Button>
-              </Card>
-            </Col>
-          </Row>
-        </TabPane>
-      </TabContent>
-
-
-            <Navbar color="faded" light>
-                <NavbarBrand href="/" className="mr-auto">Not Yo Momma's Potluck</NavbarBrand>
-                <NavbarToggler onClick={toggleNavbar} className="mr-2" />
-                <Collapse isOpen={!collapsed} navbar>
-
-                <Nav navbar>
-                    <NavItem>
-                        <NavLink href="/components/">Components</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-                    </NavItem>
-
-                </Nav>
+                        <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            AfterLoginMenu
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                            <DropdownItem>
+                            Upcoming Event1
+                            </DropdownItem>
+                            <DropdownItem>
+                            Upcoming Event2
+                            </DropdownItem>
+                            <DropdownItem divider />
+                            <DropdownItem>
+                            Logout
+                            </DropdownItem>
+                        </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
                 </Collapse>
             </Navbar>
         </div>
     )
 }
-
