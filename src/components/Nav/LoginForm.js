8993339fad265e864c9e151f79b516f7b6d67229
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-25 11:26:33 -0400

    Eliminate warnings

--- src/components/Nav/LoginForm.js
+++ src/components/Nav/LoginForm.js
@@ -1,16 +1,30 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import React from "react";
+import { Form, FormGroup, Label, Input } from "reactstrap";
 
-export default function LoginForm () {
+export default function LoginForm() {
   return (
     <Form inline>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label for="email" className="mr-sm-2">Email</Label>
-        <Input type="email" name="email" id="email" placeholder="something@idk.cool" />
+        <Label htmlFor="email" className="mr-sm-2">
+          Email
+        </Label>
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="something@idk.cool"
+        />
       </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
-        <Label for="password" className="mr-sm-2">Password</Label>
-        <Input type="password" name="password" id="password" placeholder="don't share me!" />
+        <Label htmlFor="password" className="mr-sm-2">
+          Password
+        </Label>
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="don't share me!"
+        />
       </FormGroup>
     </Form>
   );
