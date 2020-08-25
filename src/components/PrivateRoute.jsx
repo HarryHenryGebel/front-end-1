fa1bd60f661fa69633be7f71cab465b8cba377f0
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-24 23:28:05 -0400

    Convert to proper function

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-export const PrivateRoute = ({ component: Component, ...rest }) => (
-  <Route
-    {...rest}
-    render={(props) =>
-      localStorage.getItem("token") ? (
-        <Component {...props} />
-      ) : (
-        <Redirect to="/login" />
-      )
-    }
-  />
-);
+export default function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
+}
