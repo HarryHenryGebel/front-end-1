fdd99fa3d478a835ee342a2af4893000ccc9cd0c
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-24 23:32:30 -0400

    Unnecessary directory

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
-import { store } from "./store";
+import store from "./store";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
--- src/store.js
+++ src/store.js
@@ -2,4 +2,5 @@ import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import hostReducer from "../reducers/hostReducer";
 
-export const store = createStore(hostReducer, applyMiddleware(thunk));
+const store = createStore(hostReducer, applyMiddleware(thunk));
+export default store;
