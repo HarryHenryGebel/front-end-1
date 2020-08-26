74f11ec2e04751dfcc6c04baa65e65639ed74716
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-26 19:59:58 -0400

    Finish User class

--- src/classes/User.js
+++ src/classes/User.js
@@ -1,1 +1,10 @@
-export default class User {}
+export default class User {
+  constructor(userId, username, primaryEmail, firstName, lastName, imageUrl) {
+    this.userId = userId;
+    this.username = username;
+    this.primaryEmail = primaryEmail;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.imageUrl = imageUrl;
+  }
+}
