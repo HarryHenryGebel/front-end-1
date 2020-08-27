00cbb4ad81a61fdbc8da1f6a9995b91c105ab71a
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-26 20:42:34 -0400

    Add Food class

---
+++ src/classes/Food.js
@@ -0,0 +1,7 @@
+export default class Food {
+  constructor(foodId, foodName, isClaimed) {
+    this.foodId = foodId;
+    this.foodName = foodName;
+    this.isClaimed = isClaimed;
+  }
+}
--- src/classes/index.js
+++ src/classes/index.js
@@ -1,7 +1,8 @@
+import Food from "./Food";
 import HostsState from "./HostState";
 import Potluck from "./Potluck";
 import User from "./User";
 
-export { HostState, Potlock, User };
+export { Food, HostState, Potlock, User };
 
 //  LocalWords:  HostState
