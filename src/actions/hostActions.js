48d567632721ce86f8802b83c69b4e05c021b03f
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-27 17:15:29 -0400

    Harry-Approved

--- src/actions/hostActions.js
+++ src/actions/hostActions.js
@@ -8,9 +8,9 @@ export const ADD_FOOD_START = "ADD_FOOD_START";
 export const ADD_FOOD_SUCCESS = "ADD_FOOD_SUCCESS";
 export const ADD_FOOD_FAIL = "ADD_FOOD_FAIL";
 
-export const ADD_GUEST_START = "ADD_GUEST_START"
-export const ADD_GUEST_SUCCESS = "ADD_GUEST_SUCCESS"
-export const ADD_GUEST_FAIL = "ADD_GUEST_FAIL"
+export const ADD_GUEST_START = "ADD_GUEST_START";
+export const ADD_GUEST_SUCCESS = "ADD_GUEST_SUCCESS";
+export const ADD_GUEST_FAIL = "ADD_GUEST_FAIL";
 
 export const UPDATE_EVENT_START = "UPDATE_EVENT_START";
 export const UPDATE_EVENT_SUCESS = "UPDATE_EVENT_SUCESS";
@@ -21,30 +21,31 @@ export const DELETE_EVENT_SUCESS = "DELETE_EVENT_SUCESS";
 export const DELETE_EVENT_FAIL = "DELETE_EVENT_FAIL";
 
 export const addFood = (id, food) => (dispatch) => {
-  ///foods/potluck/{potluckid}/food/
+
   dispatch({ action: ADD_FOOD_START });
   axios
-    .post(`https://lre-notapotluck.herokuapp.com/foods/potluck/${id}/food`, food)
+    .post(
+      `https://lre-notapotluck.herokuapp.com/foods/potluck/${id}/food/${food.foodname}`,
+      food
+    )
     .then((res) => dispatch({ action: ADD_FOOD_SUCCESS, payload: res.data }))
     .catch((e) => dispatch({ action: ADD_FOOD_FAIL }));
 };
 export const addGuest = (id, guest) => (dispatch) => {
-  dispatch({action: ADD_GUEST_START})
+  dispatch({ action: ADD_GUEST_START });
   axios
-  .post(`https://lre-notapotluck.herokuapp.com/guests/potluck/${id}`, guest)
-  .then(res => dispatch({action: ADD_FOOD_SUCCESS, payload: res.data}))
-  .catch(e=> dispatch({action: ADD_FOOD_FAIL, payload: `${e}`}))
-  
-  
-}
+    .post(`https://lre-notapotluck.herokuapp.com/guests/potluck/${id}/${guest.fname}/${guest.lname}/${guest.primaryemail}`, guest)
+    .then((res) => dispatch({ action: ADD_FOOD_SUCCESS, payload: res.data }))
+    .catch((e) => dispatch({ action: ADD_FOOD_FAIL, payload: `${e}` }));
+};
 export const createEvent = (data) => async (dispatch) => {
   dispatch({ action: CREATE_EVENT_START });
   axios
     .post("https://lre-notapotluck.herokuapp.com/potlucks/potluck", data)
     .then(async (res) => {
       await dispatch({ action: CREATE_EVENT_SUCCESS, payload: res.data });
-      await res.data.foods.map(food => addFood(res.data.potluckid, food));
-      await res.data.guests.map(guest => addGuest(res.data.potluckid, guest));
+      await res.data.foods.map((food) => addFood(res.data.potluckid, food));
+      await res.data.guests.map((guest) => addGuest(res.data.potluckid, guest));
     })
     .catch((e) => dispatch({ action: CREATE_EVENT_FAIL, payload: `${e}` }));
 };
