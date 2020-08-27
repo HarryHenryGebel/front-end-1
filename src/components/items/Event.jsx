ee59f7b4ee60590efa6afa90531cb8a902df8cf0
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-27 10:59:54 -0400

    Removed lorempicsum

--- src/components/items/Event.jsx
+++ src/components/items/Event.jsx
@@ -133,12 +133,6 @@ function Event(props) {
   return (
     <>
       <Card>
-        <CardImg
-          top
-          width="100%"
-          src="/assets/256x186.svg"
-          alt="Card image cap"
-        />
         <CardBody>
           <CardTitle>
             <h2>{eventName}</h2>
