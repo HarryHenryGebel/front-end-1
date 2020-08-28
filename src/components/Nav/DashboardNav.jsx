ac67415fc1f470c65231c7b68c88fc50c9f87b5b
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-27 22:54:00 -0400

    correct typo deleteItem => removeItem

--- src/components/Nav/DashboardNav.jsx
+++ src/components/Nav/DashboardNav.jsx
@@ -85,7 +85,9 @@ function DashboardNav(props) {
           ))}
 
           <DropdownItem divider />
-          <DropdownItem onClick={() => localStorage.deleteItem("token")}>Logout</DropdownItem>
+          <DropdownItem onClick={() => localStorage.removeItem("token")}>
+            Logout
+          </DropdownItem>
         </DropdownMenu>
       </UncontrolledDropdown>
 
