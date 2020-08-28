b1e4426699c28036b3f16acb8330636c33b1f536
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-27 22:05:11 -0400

    Create StateHolder class

--- src/classes/index.js
+++ src/classes/index.js
@@ -2,6 +2,7 @@ import Food from "./Food";
 import Guest from "./Guest.js";
 import HostsState from "./HostState";
 import Potluck from "./Potluck";
+import StateHolder from "./StateHolder";
 import User from "./User";
 
 export { Food, Guest, HostState, Potlock, User };
