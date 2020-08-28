a7b1814dd0421717deee0d8d6b671e686034c1d7
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-28 08:27:44 -0400

    Able to register and login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import Dashboard from "./Dashboard";
 import NavBar from "./Nav/NavBar.jsx";
 import EventPage from "./EventPage";
-import { Route } from "react-router-dom";
+import PrivateRoute from './PrivateRoute'
 
 export default function App() {
   return (
     <>
       <NavBar />
       {/* Private Routes: DashBoardNavBar */}
-      <Route exact path="/" component={Dashboard} />
-      <Route path="/event/:id" render={(props) => <EventPage {...props} />} />
+      <PrivateRoute exact path="/" component={Dashboard} />
+      <PrivateRoute path="/event/:id" render={(props) => <EventPage {...props} />} />
     </>
   );
 }
