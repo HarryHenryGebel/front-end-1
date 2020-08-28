9bf9a020f2d27417c1e4200984b8f0c85bd5d8f1
Harry Henry Gebel <hhgebel@gmail.com>
2020-08-28 00:10:24 -0400

    debugging

--- src/utils/network.js
+++ src/utils/network.js
@@ -14,12 +14,15 @@ export function axiosWithAuth() {
 export async function storeLoginInformation(token, username) {
   try {
     localStorage.setItem("token", token);
+    requester.setOptions({ headers: { Authorization: `Bearer ${token}` } });
+    debugger;
     const id = requester.createUniqueID();
     await requester.get(
       "https://lre-notapotluck.herokuapp.com//users/users",
       id
     );
     const users = requester.response(id).data;
+    console.log(users);
   } catch (error) {
     console.log(error);
     throw error;
